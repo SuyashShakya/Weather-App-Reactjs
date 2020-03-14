55976e8848ee1fda26e5d091f836b6db3cf8d4b9
Suyash Shakya <suyashshakya.h0p3@gmail.com>
2020-03-14 10:49:13 +0545

    changes

--- src/component/Card.js
+++ src/component/Card.js
@@ -2,14 +2,16 @@ import React from 'react';
 import buttonBackground from '../assets/buttonBackground.jpg';
 
 const Card = (props) => {
-    const { temp, date, onClick, key } = props;
+    const { temp1, temp2, date, onClick, key } = props;
     return (
         <div key={key} data-testid="button">
-            <button style={{ width: 200, height: 200, borderRadius: 10, margin: 5, backgroundImage: `url(${buttonBackground})` }}
+            <button style={{ width: 210, height: 230, borderRadius: 10, margin: 5, backgroundImage: `url(${buttonBackground})` }}
                 onClick={onClick}
             >
-                <p style={{ color: 'white' }}>Average Temperature:</p>
-                <p style={{ color: 'white' }}>{temp}</p>
+                <p style={{ color: 'white' }}>Average Max Temperature:</p>
+                <p style={{ color: 'white' }}>{temp1}</p>
+                <p style={{ color: 'white' }}>Average Min Temperature:</p>
+                <p style={{ color: 'white' }}>{temp2}</p>
                 <p style={{ color: 'white' }}>Date:</p>
                 <p style={{ color: 'white' }}>{date}</p>
             </button>
--- src/container/Weather.js
+++ src/container/Weather.js
@@ -5,41 +5,112 @@ import { Bar } from 'react-chartjs-2';
 import back from '../assets/back.jpg';
 
 
-const DayData = [
-    { id: 1, Date: '1 March 2020', Time0: 15, Time2: 16, Time4: 17, Time6: 18, Time8: 20, Time10: 22, Time12: 25, Time14: 26, Time16: 23, Time18: 22, Time20: 19, Time22: 18 },
-    { id: 2, Date: '5 March 2020', Time0: 16, Time2: 18, Time4: 19, Time6: 20, Time8: 21, Time10: 24, Time12: 27, Time14: 28, Time16: 24, Time18: 23, Time20: 22, Time22: 20 },
-    { id: 3, Date: '10 March 2020', Time0: 18, Time2: 20, Time4: 22, Time6: 24, Time8: 26, Time10: 28, Time12: 30, Time14: 32, Time16: 28, Time18: 27, Time20: 26, Time22: 23 },
-    { id: 4, Date: '15 March 2020', Time0: 18, Time2: 20, Time4: 21, Time6: 22, Time8: 24, Time10: 25, Time12: 30, Time14: 34, Time16: 32, Time18: 28, Time20: 24, Time22: 20 },
-    { id: 5, Date: '20 March 2020', Time0: 20, Time2: 21, Time4: 22, Time6: 23, Time8: 24, Time10: 28, Time12: 32, Time14: 34, Time16: 30, Time18: 28, Time20: 25, Time22: 23 },
+// const DayData = [
+//     { id: 1, Date: '1 March 2020', Time0: 15, Time2: 16, Time4: 17, Time6: 18, Time8: 20, Time10: 22, Time12: 25, Time14: 26, Time16: 23, Time18: 22, Time20: 19, Time22: 18 },
+//     { id: 2, Date: '5 March 2020', Time0: 16, Time2: 18, Time4: 19, Time6: 20, Time8: 21, Time10: 24, Time12: 27, Time14: 28, Time16: 24, Time18: 23, Time20: 22, Time22: 20 },
+//     { id: 3, Date: '10 March 2020', Time0: 18, Time2: 20, Time4: 22, Time6: 24, Time8: 26, Time10: 28, Time12: 30, Time14: 32, Time16: 28, Time18: 27, Time20: 26, Time22: 23 },
+//     { id: 4, Date: '15 March 2020', Time0: 18, Time2: 20, Time4: 21, Time6: 22, Time8: 24, Time10: 25, Time12: 30, Time14: 34, Time16: 32, Time18: 28, Time20: 24, Time22: 20 },
+//     { id: 5, Date: '20 March 2020', Time0: 20, Time2: 21, Time4: 22, Time6: 23, Time8: 24, Time10: 28, Time12: 32, Time14: 34, Time16: 30, Time18: 28, Time20: 25, Time22: 23 },
+// ]
 
-]
-
-const noOfTime = (Object.keys(DayData[0]).length);
+// const noOfTime = (Object.keys(DayData[0]).length);
 
 
 
 const Weather = () => {
     const [timePassed, settimePassed] = useState(false);
-    const [chartData, setChartData] = useState({});
+    const [chartData1, setChartData1] = useState({});
+    const [chartData2, setChartData2] = useState({});
     const [radio, setRadio] = useState(true);
-
+    const [isLoaded, setIsLoaded] = useState(true);
+    const [items, setItems] = useState();
+    const [final_arr, setFinal_arr] = useState(null);
+    const [ind, setind] = useState(6);
     useEffect(() => {
         setTimeout(() => {
-            setTimePassed();
+            Passed();
         }, 2000);
-    },[]);
+        apiCalls();
+    }, [radio]);
 
-    const setTimePassed = () => {
+    const apiCalls = () => {
+        fetch("http://api.openweathermap.org/data/2.5/forecast?q=Kathmandu&APPID=920e416e06ad3a34c0ec275b1e96bc49")
+            .then(res => res.json())
+            .then((data) => {
+
+                setItems(data.items)
+                console.log(radio)
+                const new_array = [];
+                for (var i = 0; i < data.list.length; i++) {
+                    new_array.push({ 'Date': data.list[i].dt_txt.split(' ')[0], 'Time': data.list[i].dt_txt.split(' ')[1], 'Max_Temp': data.list[i].main.temp_max, 'Min_Temp': data.list[i].main.temp_min })
+                }
+
+                let output = [];
+                let date = "";
+                new_array.forEach(data => {
+                    let temp = {
+                        Date: "",
+                        Time: [],
+                        Max_Temp: [],
+                        Min_temp: []
+                    };
+                    if (date == data.Date) {
+                        let filter = output.filter(o => o.Date == data.Date)
+
+                        radio ? filter[0].Max_Temp.push(data.Max_Temp - 273.15)
+                            : filter[0].Max_Temp.push((data.Max_Temp - 273.15) * (9 / 5) + 32)
+
+
+                        radio ? filter[0].Min_temp.push(data.Min_Temp - 273.15)
+                            : filter[0].Min_temp.push((data.Min_Temp - 273.15) * (9 / 5) + 32)
+
+                        filter[0].Time.push(data.Time);
+                    } else {
+                        date = data.Date;
+                        temp.Date = data.Date;
+
+                        radio ? temp.Max_Temp.push(data.Max_Temp - 273.15)
+                            :
+                            temp.Max_Temp.push((data.Max_Temp - 273.15) * (9 / 5) + 32)
+
+
+                        radio ? temp.Min_temp.push(data.Min_Temp - 273.15)
+                            :
+                            temp.Min_temp.push((data.Min_Temp - 273.15) * (9 / 5) + 32)
+
+                        temp.Time.push(data.Time);
+                        output.push(temp);
+                    }
+                });
+
+                // const final_arr=[];
+                // for(var i=0;i<result.length;i++){
+                //     final_arr.push(result[i])
+                // }
+                // final_arr.splice(1, result.length - 1);
+                // console.log(result.splice(1, result.length - 1));
+                setFinal_arr(output);
+                // setFinal_arr(result)
+                setIsLoaded(false)
+            })
+            .catch(err => {
+                setIsLoaded(false)
+            })
+    }
+
+    const Passed = () => {
         settimePassed(true);
     }
     const celciusHandler = () => {
         setRadio(true)
+        apiCalls()
     }
     const farenheitHandler = () => {
         setRadio(false)
+        apiCalls()
     }
 
-    if (!timePassed) {
+    if (isLoaded) {
         return <Welcome />
     } else {
         return (
@@ -59,65 +130,69 @@ const Weather = () => {
                         />Fahrenheit
                         </div>
                     <br />
-                    {radio ?
-                        < div class="container">
-                            <div class="card-deck">
-                                {DayData.map((weather, index) => {
-                                    return (
-                                        <Card
-                                            temp={((weather.Time0 + weather.Time2 + weather.Time4 + weather.Time6 + weather.Time8 + weather.Time10 + weather.Time12 + weather.Time14 + weather.Time16 + weather.Time18 + weather.Time20 + weather.Time22) / (noOfTime - 2)).toFixed(2) + ' Degree Celcius'}
-                                            date={weather.Date}
-                                            onClick={() => (setChartData({
-                                                labels: ['12 Am', '2 Am', '4 Am', '6 Am', '8 Am', '10 Am', '12 Pm', '2 Pm', '4 Pm', '6 Pm', '8 Pm', '10 Pm'],
+
+                    < div class="container">
+                        <div class="card-deck">
+                            {!isLoaded && final_arr.map((item, index) => {
+                                var max_sum = 0;
+                                for (var i = 0; i < item.Max_Temp.length; i++) {
+                                    max_sum = max_sum + item.Max_Temp[i]
+                                }
+                                var min_sum = 0;
+                                for (var i = 0; i < item.Min_temp.length; i++) {
+                                    min_sum = min_sum + item.Min_temp[i]
+                                }
+                                return (
+                                    <Card
+                                        temp1={`${parseFloat(max_sum / item.Max_Temp.length).toFixed(3)} ${radio ? 'Celcius' : 'Fahrenheit'}`}
+                                        date={item.Date}
+                                        temp2={`${parseFloat(min_sum / item.Min_temp.length).toFixed(3)} ${radio ? 'Celcius' : 'Fahrenheit'}`}
+                                        onClick={() => (setChartData1({
+                                            labels: item.Time,
+                                            datasets: [
+                                                {
+                                                    label: 'Temperature',
+                                                    data: item.Max_Temp,
+                                                    backgroundColor: 'rgba(255, 99, 132, 0.6)'
+                                                }
+                                            ]
+                                        }),
+                                            setChartData2({
+                                                labels: item.Time,
                                                 datasets: [
                                                     {
                                                         label: 'Temperature',
-                                                        data: [weather.Time0, weather.Time2, weather.Time4, weather.Time6, weather.Time8, weather.Time10, weather.Time12, weather.Time14, weather.Time16, weather.Time18, weather.Time20, weather.Time22],
+                                                        data: item.Min_temp,
                                                         backgroundColor: 'rgba(255, 99, 132, 0.6)'
                                                     }
                                                 ]
                                             })
-
-                                            )} />
-                                    )
-                                })}
-                            </div>
+                                        )
+                                        } />
+                                )
+                            })}
                         </div>
-                        : < div class="container">
-                            <div class="card-deck">
-                                {DayData.map(weather => {
-                                    return (
-                                        <Card
-                                            temp={(((weather.Time0 + weather.Time2 + weather.Time4 + weather.Time6 + weather.Time8 + weather.Time10 + weather.Time12 + weather.Time14 + weather.Time16 + weather.Time18 + weather.Time20 + weather.Time22) / (noOfTime - 2)) * (9 / 5) + 32).toFixed(2) + ' Degree Fahrenheit'}
-                                            date={weather.Date}
-                                            onClick={() => (
-                                                setChartData({
-                                                    labels: ['12 Am', '2 Am', '4 Am', '6 Am', '8 Am', '10 Am', '12 Pm', '2 Pm', '4 Pm', '6 Pm', '8 Pm', '10 Pm'],
-                                                    datasets: [
-                                                        {
-                                                            label: 'Temperature',
-                                                            data: [(weather.Time0 * (9 / 5) + 32).toFixed(2), (weather.Time2 * (9 / 5) + 32).toFixed(2), (weather.Time4 * (9 / 5) + 32).toFixed(2), (weather.Time6 * (9 / 5) + 32).toFixed(2), (weather.Time8 * (9 / 5) + 32).toFixed(2), (weather.Time10 * (9 / 5) + 32).toFixed(2), (weather.Time12 * (9 / 5) + 32).toFixed(2), (weather.Time14 * (9 / 5) + 32).toFixed(2), (weather.Time16 * (9 / 5) + 32).toFixed(2), (weather.Time18 * (9 / 5) + 32).toFixed(2), (weather.Time20 * (9 / 5) + 32).toFixed(2), (weather.Time22 * (9 / 5) + 32).toFixed(2)],
-                                                            backgroundColor: 'rgba(255, 99, 132, 0.6)'
-                                                        }
-                                                    ]
-                                                })
+                    </div>
 
-                                            )} />)
-                                })}
-                            </div>
-                        </div>}
                     <div className="chart">
                         <br />
-                        <center> <h2>BarGraph of Temperature of Kathmandu in {radio ? 'Celcius' : 'Fahrenheit'}</h2></center>
+                        <center> <h2>BarGraph of Maximum Temperature of Kathmandu in {radio ? 'Celcius' : 'Fahrenheit'}</h2></center>
                         <br />
                         <Bar
-                            data={chartData}
+                            data={chartData1}
+                        />
+                    </div>
+                    <div className="chart">
+                        <br />
+                        <center> <h2>BarGraph of Minimum Temperature of Kathmandu in {radio ? 'Celcius' : 'Fahrenheit'}</h2></center>
+                        <br />
+                        <Bar
+                            data={chartData2}
                         />
                     </div>
                 </div>
             </div>
         )
-
 
     }
 }
